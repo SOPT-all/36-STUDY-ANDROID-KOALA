49125e80a00596478afd75f4f42e74a7fd9f6270
MinseoSONG <mins041600@naver.com>
2025-04-28 18:30:10 +0900

    [week1] 백준_bronze_빠른A+B

---
+++ minseo-song/week1/백준_bronze_빠른A+B.kt
@@ -0,0 +1,17 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.StringTokenizer
+
+fun main(){
+    val bufferedReader = BufferedReader(InputStreamReader(System.`in`))
+    val bufferedWriter = BufferedWriter(OutputStreamWriter(System.out))
+
+    repeat(bufferedReader.readLine().toInt()){
+        val str = StringTokenizer(bufferedReader.readLine())
+        bufferedWriter.write((str.nextToken().toInt() + str.nextToken().toInt()).toString()+"\n")
+    }
+    bufferedReader.close()
+    bufferedWriter.flush()
+}
