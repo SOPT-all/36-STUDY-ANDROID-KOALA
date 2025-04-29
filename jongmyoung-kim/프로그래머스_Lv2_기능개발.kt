9fe5df811aa06bcfd3abba5c45b44fe477e20f07
Jong <jm991014@gmail.com>
2025-04-29 13:35:20 +0900

    [1주차] 프로그래머스_Lv2_기능개발_김종명

---
+++ jongmyoung-kim/프로그래머스_Lv2_기능개발.kt
@@ -0,0 +1,25 @@
+import kotlin.math.ceil
+
+fun solution(progresses: IntArray, speeds: IntArray): IntArray {
+    var answer = intArrayOf()
+    val list = mutableListOf<Int>()
+
+    progresses.forEachIndexed { index, progress ->
+        val timeTaken = ceil((100 - progress).toFloat() / speeds[index]).toInt()
+        list.add(timeTaken)
+    }
+
+    var count = 0
+    var current = list[0]
+
+    list.forEach {
+        if (it <= current) count++
+        else {
+            answer += count
+            current = it
+            count = 1
+        }
+    }
+    answer += count
+    return answer
+}
