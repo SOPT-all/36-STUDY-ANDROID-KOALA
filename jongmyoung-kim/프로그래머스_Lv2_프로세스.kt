49b22f3e7f817ebf87e1029e877b29ea7b442549
Jong <jm991014@gmail.com>
2025-04-29 17:32:51 +0900

    [1주차] 프로그래머스_Lv2_프로세스_김종명

---
+++ jongmyoung-kim/프로그래머스_Lv2_프로세스.kt
@@ -0,0 +1,20 @@
+import java.util.*
+
+fun solution(priorities: IntArray, location: Int): Int {
+    var answer = 0
+    val readyQueue = LinkedList<Pair<Int, Int>>().apply {
+        addAll(priorities.mapIndexed { index, priority -> priority to index })
+    }
+    val jobQueue = LinkedList<Int>().apply { addAll(priorities.sortedDescending()) }
+
+    while (readyQueue.isNotEmpty()) {
+        val cur = readyQueue.poll()
+        if (cur.first != jobQueue.peek()) readyQueue.offer(cur)
+        else {
+            jobQueue.poll()
+            answer++
+            if (location == cur.second) break
+        }
+    }
+    return answer
+}
